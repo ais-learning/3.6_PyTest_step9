db1d8bce954e9b41c5231d7882ef0e7a7bb730e9
Ais <sanikeevaa@gmail.com>
2019-07-23 12:48:05 -0400

    Minor changes in test_items

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_backet_btn_is_present(browser):
     browser.get(link)
     # time.sleep(15) 
-    browser.find_element_by_class_name("btn-add-to-basket")
+    btn = browser.find_element_by_class_name("btn-add-to-basket")
+    assert btn.is_displayed()
